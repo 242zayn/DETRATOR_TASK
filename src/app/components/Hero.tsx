181ab51cfa1f0e17e965e085cbdeb0d4f55e9ea7
242zayn <sarveshsharma@gmail.com>
2025-01-22 11:28:42 +0530

    Work on Responsive

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,24 +5,29 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 
 const ResponsiveComponents = () => {
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isXs = useMediaQuery(theme.breakpoints.down("xs"));
+  const isSm = useMediaQuery(theme.breakpoints.between("xs", "sm"));
+  const isMd = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isLg = useMediaQuery(theme.breakpoints.between("md", "lg"));
+  const isXl = useMediaQuery(theme.breakpoints.up("lg"));
 
   return (
-    <Box
+    <Boxst
       display="flex"
-      flexDirection={isSmallScreen ? "column" : "row"}
-      justifyContent={isSmallScreen ? "center" : "flex-start"}
+      flexDirection={isXs || isSm ? "column" : "row"}
+      justifyContent={isXs || isSm ? "center" : "flex-start"}
       alignItems="center"
       gap={2}
       paddingTop={2}
-      paddingRight={12}
+      paddingRight={isXs || isSm ? 2 : 12}
+      paddingLeft={isXs || isSm ? 2 : 12}
     >
-      {!isSmallScreen && <Box width="20%" />}
+      {!isXs && !isSm && <Box width="15%" />}
       <Box
-        width={isSmallScreen ? "100%" : "45%"}
+        width={isXs || isSm ? "100%" : "50%"}
         height={200}
         bgcolor="lightgray"
-        marginBottom={isSmallScreen ? 2 : 0}
+        marginBottom={isXs || isSm ? 2 : 0}
         display="flex"
         justifyContent="center"
         alignItems="center"
@@ -30,18 +35,17 @@ const ResponsiveComponents = () => {
         <p>First Component (Centered)</p>
       </Box>
       <Box
-        width={isSmallScreen ? "100%" : "35%"}
+        width={isXs || isSm ? "100%" : "35%"}
         height={200}
         bgcolor="lightgray"
         display="flex"
         justifyContent="center"
         alignItems="center"
-        marginTop={isSmallScreen ? 2 : 0}
+        marginTop={isXs || isSm ? 2 : 0}
       >
         <p>Second Component (Right)</p>
       </Box>
     </Box>
-    // </Container>
   );
 };
 
